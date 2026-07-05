5dbf31f0df1d37f5d0e4fee9c5bec2c3ad7f669d
Asif Abdullah Munna <munna@Asifs-MacBook-Air.local>
2026-07-06 02:39:55 +0600

    Refactored analytics into separate page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Syllabus from "./pages/Syllabus";
 import StudyPlanner from "./pages/StudyPlanner";
 import MCQ from "./pages/MCQ";
 import Mentor from "./pages/Mentor";
+import Analytics from "./pages/Analytics";
 const stats = [
   { title: "Today's Study", value: "0 min" },
   { title: "XP", value: "0" },
@@ -64,63 +65,7 @@ export default function App() {
 
 {page === "mentor" && <Mentor />}
 
-{page === "analytics" && (
-  <>
-    <h1>Analytics Dashboard</h1>
-
-    <div className="cards">
-
-      <div className="card">
-        <h3>Total Study</h3>
-        <h2>125 Hours</h2>
-      </div>
-
-      <div className="card">
-        <h3>Current Streak</h3>
-        <h2>7 Days 🔥</h2>
-      </div>
-
-      <div className="card">
-        <h3>XP Earned</h3>
-        <h2>3,250 XP</h2>
-      </div>
-
-      <div className="card">
-        <h3>Rank</h3>
-        <h2>Cadet ⭐</h2>
-      </div>
-
-    </div>
-
-    <div className="card" style={{ marginTop: "20px" }}>
-      <h2>Weekly Progress</h2>
-
-      <div
-        style={{
-          height: "20px",
-          background: "#102c6b",
-          borderRadius: "10px",
-          overflow: "hidden",
-          marginTop: "15px"
-        }}
-      >
-        <div
-          style={{
-            width: "72%",
-            height: "100%",
-            background: "#4CAF50"
-          }}
-        />
-      </div>
-
-      <p style={{ marginTop: "15px" }}>
-        72% Weekly Goal Completed
-      </p>
-
-    </div>
-  </>
-)}
-
+{page === "analytics" && <Analytics />}
       </main>
 
     </div>
